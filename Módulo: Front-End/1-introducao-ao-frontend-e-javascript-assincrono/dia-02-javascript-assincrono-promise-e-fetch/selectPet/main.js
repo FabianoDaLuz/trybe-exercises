ddc984e39f18eaafce5b3ad8d583968733606f06
FabianoDaLuz <escottofabiano@gmail.com>
2023-06-27 21:51:18 -0300

    select pet

---
+++ Módulo: Front-End/1-introducao-ao-frontend-e-javascript-assincrono/dia-02-javascript-assincrono-promise-e-fetch/selectPet/main.js
@@ -0,0 +1,35 @@
+const dogButton = document.querySelector('#random-dog');
+const petImage = document.querySelector('#random-pet-image');
+const catButton = document.querySelector('#random-cat')
+const surpriseMeButton = document.querySelector('#surprise-me');
+
+dogButton.addEventListener('click', () => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(res => res.json())
+        .then(data => {
+            const petURL = data.message;
+            petImage.src = petURL;
+        });
+
+    catButton.addEventListener('click', () => {
+        fetch('https://api.thecatapi.com/v1/images/search')
+            .then((res) => res.json())
+            .then(([data]) => {
+                const petURL = data.url;
+                petImage.src = petURL;
+            });
+    });
+
+    surpriseMeButton.addEventListener("click", () => {
+        Promise.any([
+            fetch("https://api.thecatapi.com/v1/images/search"),
+            fetch("https://dog.ceo/api/breeds/image/random"),
+        ])
+            .then((res) => res.json())
+            .then((data) => {
+                const petURL = data.message || data[0].url;
+                petImage.src = petURL;
+            });
+    });
+});
+
