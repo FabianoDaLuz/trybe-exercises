aeea4998bd28aadef2ad3f13a8c417116f708639
FabianoDaLuz <escottofabiano@gmail.com>
2023-07-13 16:57:05 -0300

    project deleted

---
+++ Módulo: Front-End/2-introducao-ao-react/dia-01-introducao-ao-react-e-ao-typescript/README.md
@@ -0,0 +1,4 @@
+A principal diferença em .js e .ts é que 
+ em typescrypt todas as variáveis e constantes apresentam tipos definidos
+
+ Aprendendo typescript
---
+++ Módulo: Front-End/2-introducao-ao-react/dia-01-introducao-ao-react-e-ao-typescript/examples.js
@@ -0,0 +1,10 @@
+const a = '1';
+const b = 2;
+
+function sum(x, y) {
+    return x + y
+}
+
+console.log(sum(a, b))
+// retorna "12" (string) ap invés de 3(number)
+
