dae0be746e7628c77211a48f169c112a90b497af
FabianoDaLuz <escottofabiano@gmail.com>
2023-07-13 16:49:25 -0300

    2.1

---
+++ Módulo: Front-End/2-introducao-ao-react/dia-01-introducao-ao-react-e-ao-typescript/projeto/src/components/Title/Title.tsx
@@ -0,0 +1,11 @@
+import './Title.css';
+
+const Title = () => {
+    const nome = 'Thiago Frames';
+
+    return (
+        <p>Hello {`${nome} - o brabo do CSS`}</p>
+    )
+}
+
+export default Title;
