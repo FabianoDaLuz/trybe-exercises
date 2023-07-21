5957fb553897a3b024cf934eaee30d5be3665cf0
FabianoDaLuz <escottofabiano@gmail.com>
2023-07-21 15:54:45 -0300

    redoing

---
+++ Módulo: Front-End/2-introducao-ao-react/dia-01-introducao-ao-react-e-ao-typescript/App.tsx
@@ -0,0 +1,7 @@
+import Header from './Header';
+
+function App() {
+    return ''
+}
+
+export default App;
--- Módulo: Front-End/2-introducao-ao-react/dia-01-introducao-ao-react-e-ao-typescript/examples.js
+++ Módulo: Front-End/2-introducao-ao-react/dia-01-introducao-ao-react-e-ao-typescript/examples.js
@@ -7,3 +7,19 @@ function sum(x, y) {
 
 console.log(sum(a, b))
 // retorna "12" (string) ap invés de 3(number)
+
+function Header() {
+    return (
+        <h1>Olá mundo!</h1>
+    );
+}
+
+// JSX
+function jsxHeader() {
+    const name = "Trybe";
+
+    return (
+        <h1>Olá {name}</h1>
+    );
+}
+
